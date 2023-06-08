2b1577cf44f23c7f86cc650129f6b5b81ce2393d
Divyesh <91273646+DivyeshSuhagiya@users.noreply.github.com>
2023-06-08 17:04:17 +0530

    Delete route directory

--- route/index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const productRoutes = require('./productRoute');
-const userRoutes = require('./userRoute');
-router.use('/product' , productRoutes);
-router.use('/user' , userRoutes);
-
-module.exports = router;
