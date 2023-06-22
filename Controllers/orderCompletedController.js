2911a311d9b46342fa4a2b206b70e0b5aa6dbb00
Divyesh <91273646+DivyeshSuhagiya@users.noreply.github.com>
2023-06-22 12:10:52 +0530

    Update orderCompletedController.js

--- Controllers/orderCompletedController.js
+++ Controllers/orderCompletedController.js
@@ -21,7 +21,7 @@ exports.order = {
               const user = await ORDER_COMPLETED.findOne({ userId: decoded.user_Id });
               let completedRecords = records.orderProducts.filter(x => x.orderComplateTime < new Date())
             
-              await ORDER.update( 
+              await ORDER.update(   
                   { userId: decoded.user_Id },
                   {
                       $pull: {
@@ -32,27 +32,28 @@ exports.order = {
               );
 
             if(completedRecords.length > 0){
+              const user = await ORDER_COMPLETED.findOne({ userId: decoded.user_Id });
               if(user){
-                  await ORDER_COMPLETED.updateOne({ userId: decoded.user_Id }, { $push : { orderCompletedProducts: {$each: completedRecords} } })
+                await ORDER_COMPLETED.updateOne({ userId: decoded.user_Id }, { $push : { orderCompletedProducts: {$each: completedRecords, $slice: -10   }} })
               }else{
                 await ORDER_COMPLETED.create({ userId: decoded.user_Id , orderCompletedProducts: completedRecords })
               }
             }
           }
-          const user = await ORDER_COMPLETED.findOne({ userId: decoded.user_Id });
-          if(user && user?.orderCompletedProducts?.length > 10){
-            const slicedArray = user?.orderCompletedProducts.slice(0 , user?.orderCompletedProducts?.length-10)?.map(x => x._id);
-            await ORDER_COMPLETED.update(
-              {_id: user._id},
-              {"$pull":{"orderCompletedProducts":{"_id":{$in:slicedArray}}}}
-            )
-          }
-
-          const orders = await ORDER_COMPLETED.find({ userId : decoded.user_Id});
+          // const user = await ORDER_COMPLETED.findOne({ userId: decoded.user_Id });
+          // if(user && user?.orderCompletedProducts?.length > 10){
+          //   const slicedArray = user?.orderCompletedProducts.slice(0 , user?.orderCompletedProducts?.length-10)?.map(x => x._id);
+          //   console.log(slicedArray)
+          //   await ORDER_COMPLETED.update(
+          //     {_id: user._id},
+          //     {"$pull":{"orderCompletedProducts":{"_id":{$in:slicedArray}}}}
+          //   )
+          // }
+          const orders = await ORDER_COMPLETED.findOne({ userId : decoded.user_Id});
           return res.json({
             message: "Your data get successfull",
             isSuccess: true,
-            data: orders
+            data: orders?.orderCompletedProducts ?? []
           });
         }
       });
